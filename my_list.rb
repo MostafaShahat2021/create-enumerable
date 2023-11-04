cec7b030c06f865d9e8579cc0c77f4e00d5596de
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-11-04 10:48:20 +0200

    verify test solution

--- my_list.rb
+++ my_list.rb
@@ -12,3 +12,11 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts list.all? { |e| e < 5 }
+
+puts list.any? { |e| e == 2 }
+
+puts list.filter { |e| e.even? }
